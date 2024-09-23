8408ca82d07d246676f738437a8f32dcd80c0341
Sialovesanimals123 <162298429+Sialovesanimals123@users.noreply.github.com>
2024-09-23 18:29:21 +1000

    Add files via upload
    im the best >:D

---
+++ scetch.js
@@ -0,0 +1,9 @@
+function openProjects(){
+    window.location.replace("project.html")   
+}
+function opencontacts(){
+    window.location.replace("contact.html")    
+}
+function home(){
+    window.location.replace("index.html")
+}
